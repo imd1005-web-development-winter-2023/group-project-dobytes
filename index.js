d4daa2e1652618107dd130f3f4f4cdaa84b9d047
sarah <sarah.naseer20@gmail.com>
2023-04-11 01:32:39 -0400

    update
    Co-authored-by: Lenibi <Lenibi@users.noreply.github.com>
    Co-authored-by: Akhil Penakacherla <pena0037@algonquinlive.com>

--- index.js
+++ index.js
@@ -27,9 +27,37 @@ let colourWalkSprites = [
     "./sprites/Brown/brownrun.gif", 
     "./sprites/LightBlue/lightbluerun.gif",
     "./sprites/Pink/pinkrun.gif",
-    "./sprites/green/greenrun.gif"];
+    "./sprites/green/greenrun.gif",
+    "/sprites/Red/redrun.gif"];
 
+let spawnCounter = 0;
+let randomSpawnRate = 0;
+
+let flyingIndex = 0;
+let flyingArray = [];
+let xArray = [];
+let speedArray=[];
 timer1 = setInterval(timerFunction, 10);
 function timerFunction(){
-    
+    spawnCounter+=0.1;
+   if (spawnCounter>randomSpawnRate){
+    //get a new spawnrate
+    randomSpawnRate = Math.floor(Math.random() * 100) + 50;
+    //instantiate new among us into array to access later
+    flyingArray.append(document.createElement("img"));
+    xArray.append(-15);
+    //setting the image
+    flyingArray[flyingIndex]=Math.floor(Math.random() * 6);
+    //set the size
+    let randomSize = Math.floor(Math.random() * 15) + 10;
+    flyingArray[flyingIndex].style.height = randomSize+"vh";
+    flyingArray[flyingIndex].style.width = randomSize+"vh";
+    //increase index
+    flyingIndex++;
+   }
+   for(let i=0;i<flyingIndex;i++){
+    xArray[i]+=
+    flyingArray[i].style.left=xArray[]
+   }
+
 }
